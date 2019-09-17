58403dca7231be12e22e9810ead01dca99436f63
Jason Calk <jasoncalk@gmail.com>
2019-09-17 18:39:24 -0500

    Started working on Table Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import HeaderComponent from './containers/wrapper/header/HeaderComponent';
 import FooterComponent from './containers/wrapper/footer/FooterComponent';
 import HomeComponent from './containers/home/Home Component';
+import CompareComponent from './containers/compare/CompareComponent';
 import SideDrawer from './containers/wrapper/sideDrawer/SideDrawer';
 import Backdrop from './containers/wrapper/backdrop/Backdrop';
+
 // import CompareComponent from './containers/compare/CompareComponent';
 
 import './App.css';
@@ -39,24 +42,27 @@ class App extends Component {
       <div className="App">
          <SideDrawer show={this.state.sideDrawerOpen} />
          {backdrop}
-         
+{/* Header for the entire website */}
         <header className="App-header">
           <HeaderComponent drawerClickHandler={this.drawerToggleClickHandler} />
         </header>
-        <div className="Container"> 
-          <div className="App-homeBody">
-            <HomeComponent />
-          </div>
 
-          {/* <div className="scComponent">
-            <CompareComponent>
-              
-            </CompareComponent>
+{/* Body for the entire website */}
+{/* Router Open */}
+        <div className="Container"> 
+          {/* <div className="App-homeBody">            
+            <HomeComponent />
           </div> */}
+          <div className="App-compareBody">
+            <CompareComponent />
+          </div>
         </div>
+{/* Router Close */}
+{/* Footer for the entire website */}
         <footer className="App-footer>">
           <FooterComponent />
         </footer> 
+{/* Closes the render for the app */}
       </div>
     );
   }
---
+++ src/containers/compare/CompareComponent.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './CompareStyles.css';
+
+export default class CompareComponent extends Component {
+    constructor(props) {
+      super(props);
+    };
+
+    render () {
+        return (
+            <div>
+                Compare Component Works
+            </div>
+        )
+    }
+
+};
+
